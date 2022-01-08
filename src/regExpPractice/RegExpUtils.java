7db6013ad85fff5cde03c1714a208fcaf560cc9b
DESKTOP-SCNM2G2\CHO <chominhyeong@gmail.com>
2022-01-08 23:54:10 +0900

    정규표현식 소스에 새로 이미지, 링크 URL 삽입 로직 구현 완료

--- src/regExpPractice/RegExpUtils.java
+++ src/regExpPractice/RegExpUtils.java
@@ -26,7 +26,7 @@ public class RegExpUtils {
 		while(matcher.find()) {										// REGEX에 해당하는 문자열을 찾았다면 TRUE 반환 (= url형태 TEXT가 감지되지 않을 때까지 반복 = url 변환 완료할 때까지 반복)
 			String compare = matcher.group();						// matcher.group() -> 감지한 URL TEXT 반환
 			if(compare.contains("pcImg")) {							// 1) 기존 소스에서 "pcImg"가 존재할 경우
-				matcher.appendReplacement(buffer, replacePcImg);		// -> buffer에 chngPcImg 대체하여 추가
+				matcher.appendReplacement(buffer, replacePcImg);	// -> buffer에 chngPcImg 대체하여 추가
 			} else if(compare.contains("mobImg")) {					// 2) 기존 소스에서 "mobImg"가 존재할 경우
 				matcher.appendReplacement(buffer, replaceMobImg);	// -> buffer에 chngMobImg 대체하여 추가
 			} else if(compare.contains("mid.ebs.co.kr") || compare.contains("primary.ebs.co.kr")) {
@@ -36,5 +36,53 @@ public class RegExpUtils {
 		matcher.appendTail(buffer);									// 대체할 필요가 없는 modifySrc의 부분 추가하여 마무리
 		
 		return buffer.toString();									// 대체 완료한 buffer를 String 형태로 변환하여 return
+	}
+	
+	/**
+	 * 새 source insert
+	 * @ReferencePage URL 정규 표현식 - https://zetawiki.com/wiki/URL_%EC%A0%95%EA%B7%9C%ED%91%9C%ED%98%84%EC%8B%9D
+	 * @ReferencePage https://coding-factory.tistory.com/529
+	 * @ReferencePage https://gngsn.tistory.com/54
+	 * */
+	public static String insertUrlPatternInHtmlSource(String htmlSrc, String url, String pcImg, String mobImg) {
+		StringBuffer buffer = new StringBuffer();
+		
+		final String REGEX = "(<img\\b[^>](src=(\"\"|\'\')))|(<area\\b[^>](href=(\"\"|\'\')))"; // result -> [<img src=''][<area href=""][<img src=""][<area href=""]
+		
+		Pattern pattern = Pattern.compile(REGEX);
+		Matcher matcher = pattern.matcher(htmlSrc);
+		
+//		int cnt = 0;
+//		
+//		while(matcher.find()) {
+//			String compare = matcher.group();
+//			String lastText = compare.substring(compare.length()-1);
+//			if(compare.contains("img src")) {
+//				compare = compare.substring(0, compare.length()-1);
+//				compare = compare + (cnt == 0 ? pcImg : mobImg) + lastText;
+//			} else if(compare.contains("area href")) {
+//				compare = compare.substring(0, compare.length()-1);
+//				compare = compare + url + lastText;
+//			}
+//			matcher.appendReplacement(buffer, compare);
+//			cnt++;
+//		}
+//		matcher.appendTail(buffer);	
+		
+		for(int i=0; matcher.find(); i++) {
+			String compare = matcher.group();
+			String lastText = compare.substring(compare.length()-1);
+			if(compare.contains("img src")) {
+				compare = compare.substring(0, compare.length()-1);
+				compare = compare + (i == 0 ? pcImg : mobImg) + lastText;
+			} else if(compare.contains("area href")) {
+				compare = compare.substring(0, compare.length()-1);
+				compare = compare + url + lastText;
+			}
+			matcher.appendReplacement(buffer, compare);
+		}
+		matcher.appendTail(buffer);	
+		
+		return buffer.toString();
 	}
 }
